221e9881713b91c9a38b09925bff60633a6ea046
Amayika <amayika@Saileshs-Air.fritz.box>
2019-06-06 10:45:17 +0200

    updated code

--- Scala/Examples/CompositeExample_Presentation.scala
+++ Scala/Examples/CompositeExample_Presentation.scala
@@ -5,7 +5,7 @@ import scroll.internal.support.DispatchQuery
 import java.util.ArrayList
 import java.util.HashMap
 
-object CompositeExample extends App {
+object CompositeExample_Presentation extends App {
 
   class CompositeCompartment() extends Compartment {
 
@@ -13,12 +13,6 @@ object CompositeExample extends App {
     private val dchildren: HashMap[Directory, Child] = new HashMap
     private val fchildren: HashMap[File, Child] = new HashMap
     var root: Parent = null
-
-
-    def addRootParent(p: Parent): Unit = {
-      if (p!= null)
-        root = p
-    }
 
     /*Parent class
     Add(): Create the list of child objects and add them
@@ -119,60 +113,33 @@ object CompositeExample extends App {
   dir1.setSize(1000)
   val dir2 = new Directory("dir2")
   dir2.setSize(200)
-  val dir3 = new Directory("dir3")
-  dir3.setSize(500)
 
   val file1 = new File("file1", 70)
   val file2 = new File("file2", 30)
-  val file3 = new File("file3", 50)
-  val file4 = new File("file4", 250)
+  val file3 = new File("file3", 250)
 
   new CompositeCompartment {
     val parent1 = new Parent
     val parent2 = new Parent
-    val parent3 = new Parent
     val fileChild1 = new Child
     val fileChild2 = new Child
-    val fileChild3 = new Child
 
     dir1 play parent1
     dir2 play parent2
-    dir3 play parent3
 
     +dir1 add parent2
-    +dir1 add parent3
 
     file1 play fileChild1
     file2 play fileChild2
-    file3 play fileChild3
-
-    //For testing roles working or not
-    /*
-    println(+dir2 add fileChild1)
-    println(+dir3 add fileChild2)
-
-    println("Directory1 is playing Parent:" + (+dir1).isPlaying[Parent])
-    println("Directory2 is playing child:" + (+dir2).isPlaying[Child])
-    println("Directory3 is playing child:" + (+dir3).isPlaying[Child])
-    println("Directory2 is playing child:" + (+dir2).isPlaying[Parent])
-    println("Directory3 is playing child:" + (+dir3).isPlaying[Parent])
-
-    println("size of dir1: " + dir1.getSize)
-    println("size of file1: " + file1.getSize)
-
-
-    //println(+dir1 add parent2)
-    //println(+dir1 add parent3)
-    */
 
     //Current size of the directory
     var totalSize: Int = +dir1 sum()
     println("Total size of root directory: " + totalSize)
 
     / Add new file, resize & show /
-    val fileChild4 = new Child
-    file4 play fileChild4
-    +dir2 add file4
+    val fileChild3 = new Child
+    file3 play fileChild3
+    +dir2 add file3
     println("")
     println("New file has added----------")
     var totalNewSize: Int = +dir1 sum()
